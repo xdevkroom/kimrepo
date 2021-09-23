54b7c6e4a2566ae81eb29ba94b09be1aab30c2ce
xdevkroom <35851708+xdevkroom@users.noreply.github.com>
2021-09-23 15:23:56 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,74 @@
 from kivy.app import App
-from kivy.uix.textinput import TextInput
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from kivy.uix.image import Image
+from plyer import gps,vibrator,tts,battery
 from kivymd.toast import toast
-import os
-class app(App):
-   
+from kivy.network.urlrequest import UrlRequest
+import requests
+import arabic_reshaper
+import bidi.algorithm
+import  webbrowser
+from gtts import gTTS as gts
+import pygame
+pygame.init()
+pygame.mixer.music.load("mono.mp3")
+
+
+g = gts("مرحبا بك في كيفي موبايل لتطوير تطبيقات الاندرويد",lang="ar")
+g.save("mono.mp3")
+url="http://192.168.43.249/LED=ON"
+a = str(battery.status)
+
+class myapp(App):
+    
+    #tts.speak("hello kivymd مرحبا you welcome")
+    toast(a)
     def build(self):
-        self.info= """((Welcome to WhatsDirct))\nKSA version\nto use this app \nEnter phon number \nfor example:\n05XXXXXXXX"""
-    
-        box = FloatLayout()
-        leb = Label(text= self.info,pos=(600,1500),size_hint=(0.1,.1))
+        #webbrowser.open("http://www.google.com")
         
-        button = Button(text="Start WhatsApp Chat",size_hint=(1,.08),pos=(1,800),on_press=self.click)
+        self.text = "مرحبا"
+        self.shape = arabic_reshaper.reshape(self.text)
+        self.bi = bidi.algorithm.get_display(self.shape)
+        b1=BoxLayout(orientation="vertical")
+            
+        self.textin=TextInput(text=self.bi,input_type='number',multiline=False,font_name="arial")
+        self.label = Label(text=self.bi,font_name="arial")
+        butn1= Button(text=self.bi,on_press=self.ledon,font_name="arial")
+        butn2= Button(text="Play",on_press=self.xplay)
+        butn3= Button(text="Stop",on_press=self.xstop)
+        butn4= Button(text="Quick",on_press=self.press)
+  
+        b1.add_widget(self.textin)
+        b1.add_widget(self.label)
+        b1.add_widget(butn1)
+        b1.add_widget(butn2)
+        b1.add_widget(butn3)
+        b1.add_widget(butn4)
         
-        img = Image(source="back.jpg",allow_stretch=True,size_hint=(2,2),pos=(-700,0))
-        
-        self.textinput = TextInput(input_type='number',input_filter="int",size_hint=(1,.1),border=(2,2,2,2),font_size="33sp",multiline=False,pos=(1,900))
-        
-        box.add_widget(img)
-        box.add_widget(leb)
-        box.add_widget(self.textinput)
-        box.add_widget(button)
-      
-        return  box
-    def click(self,instance):
-        toast("Starting...")
-        number = str(self.textinput.text)
-        number1="966"
-        os.system(f"am start -a android.intent.action.VIEW -d https://api.whatsapp.com/send?phone={number1}{number} com.whatsapp")
        
-        
+        return b1
+    def press(self,instance):
+        vibrator.vibrate()
+        text = self.textin.text
+        shape = arabic_reshaper.reshape(text)
+        bi = bidi.algorithm.get_display(shape)
+        self.label.text=bi
+        toast(self.textin.text)
+    def ledon(self,instance):
+        self.b = UrlRequest(url,verify=False)
+    def xplay(self,instance):
+        if pygame.mixer.music.get_busy():
+            pass
+        else:
+            pygame.mixer.music.play()
+    def xstop(self,instance):
+        pygame.mixer.music.stop()
     
-if __name__=="__main__":
-    app().run()
+            
+    
+
+if __name__=='__main__':
+     myapp().run()
+     
